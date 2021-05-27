20a317e9870898b2fb9cc1ee552a89fcaa200555
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-05-27 09:29:11 +0300

    update

--- src/app/pages/AdminPage/CandidateTable/types/react-table-config.d.ts
+++ src/app/pages/AdminPage/CandidateTable/types/react-table-config.d.ts
@@ -65,6 +65,7 @@ declare module 'react-table' {
 			// note that having Record here allows you to add anything to the options, this matches the spirit of the
 			// underlying js library, but might be cleaner if it's replaced by a more specific type that matches your
 			// feature set, this is a safe default.
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			Record<string, any> {}
 
 	export interface Hooks<
@@ -115,8 +116,8 @@ declare module 'react-table' {
 			UseSortByColumnProps<D> {}
 
 	export interface Cell<
-		D extends Record<string, unknown> = Record<string, unknown>,
-		V = any
+		D extends Record<string, unknown> = Record<string, unknown>
+		// V = any
 	> extends UseGroupByCellProps<D>,
 			UseRowStateCellProps<D> {}
 
